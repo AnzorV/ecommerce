1e280a054fedbec320b9de03c09f102cf963fcaa
AnzorV <anzorv@gmail.com>
2024-09-03 17:37:51 +0100

    master

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -31,22 +31,35 @@ class SettingsScreen extends StatelessWidget {
             ),
             ),
             
-             Padding(
+              Padding(
               padding: const EdgeInsets.all(TSizes.defaultSpace),
               child: Column(
                children: [
                  const TSectionHeading(title: 'Account Settings', showActionButton: false,),
                  const SizedBox(height: TSizes.spaceBtwItems,),
 
-                 TSettingsMenuTile(icon: Iconsax.safe_home, title: 'My Addresses', subtitle: 'Set shopping delivery address'),
-                 TSettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subtitle: 'Add, remove products and move to checkout'),
-                 TSettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subtitle: 'In-progress and Completed Orders'),
-                 TSettingsMenuTile(icon: Iconsax.bank, title: 'Bank Account', subtitle: 'Withdraw balance to registered bank account'),
-                 TSettingsMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subtitle: 'List of all the discounted coupons'),
-                 TSettingsMenuTile(icon: Iconsax.notification, title: 'Notifications', subtitle: 'Set any kind of notification message'),
-                 TSettingsMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subtitle: 'Manage data usage and connected accounts'),
+                 const TSettingsMenuTile(icon: Iconsax.safe_home, title: 'My Addresses', subtitle: 'Set shopping delivery address'),
+                 const TSettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subtitle: 'Add, remove products and move to checkout'),
+                 const TSettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subtitle: 'In-progress and Completed Orders'),
+                 const TSettingsMenuTile(icon: Iconsax.bank, title: 'Bank Account', subtitle: 'Withdraw balance to registered bank account'),
+                 const TSettingsMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subtitle: 'List of all the discounted coupons'),
+                 const TSettingsMenuTile(icon: Iconsax.notification, title: 'Notifications', subtitle: 'Set any kind of notification message'),
+                 const TSettingsMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subtitle: 'Manage data usage and connected accounts'),
 
-               SizedBox(height: TSizes.s,)
+               const SizedBox(height: TSizes.spaceBtwSections,),
+                 const TSectionHeading(title: 'App Settings', showActionButton: false,),
+                 const SizedBox(height: TSizes.spaceBtwItems,),
+                 const TSettingsMenuTile(icon: Iconsax.document_upload, title: 'Load Data', subtitle: 'Upload data to your Cloud Firebase'),
+                 TSettingsMenuTile(icon: Iconsax.location, title: 'Geolocation', subtitle: 'Set recommendation based on location', trailing: Switch(value: true, onChanged: (value) {})),
+                 TSettingsMenuTile(icon: Iconsax.security_user, title: 'Safe Mode', subtitle: 'Search result is safe for all ages', trailing: Switch(value: false, onChanged: (value) {})),
+                 TSettingsMenuTile(icon: Iconsax.location, title: 'HD Image Quality', subtitle: 'Set image quality to be seen', trailing: Switch(value: false, onChanged: (value) {})),
+              const SizedBox(height: TSizes.spaceBtwSections,),
+                 SizedBox(
+                   width: double.infinity,
+                   child: OutlinedButton(onPressed: (){}, child: const Text('Logout')),
+                 ),
+                 const SizedBox(height: TSizes.spaceBtwSections * 2.5),
+
                ],
               ),
             ),
