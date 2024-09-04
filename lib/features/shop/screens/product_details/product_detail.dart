6fa85e6f6cda2f18e82c1dc242339a877f4d771e
AnzorV <anzorv@gmail.com>
2024-09-04 19:25:47 +0100

    master

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,9 +1,14 @@
+import 'package:ecommerceshop/common/widgets/appbar/appbar.dart';
+import 'package:ecommerceshop/common/widgets/icons/t_circular_icon.dart';
 import 'package:ecommerceshop/common/widgets/images/t_rounded_image.dart';
+import 'package:ecommerceshop/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:ecommerceshop/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:ecommerceshop/utils/constants/colors.dart';
 import 'package:ecommerceshop/utils/constants/image_strings.dart';
 import 'package:ecommerceshop/utils/constants/sizes.dart';
 import 'package:ecommerceshop/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 
@@ -13,52 +18,56 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return  Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TCurvedEdgesWidget(child: Container(
-              color: dark ? TColors.darkerGrey : TColors.light,
-              child:   Stack(
-                children: [
-                  const SizedBox(
-                      height: 400,
-                      child: Padding(
-                      padding: EdgeInsets.all(TSizes.productImageRadius * 2 ),
-                        child: Center(child: Image(image: AssetImage(TImages.productImage1))),
-                  ),
-                  ),
-                  Positioned(
-                    right: 0,
-                    bottom: 30,
-                    left: TSizes.defaultSpace,
-                    child: SizedBox(
-                      height: 80,
-                      child: ListView.separated(
-                        shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          separatorBuilder: (_, __) => const SizedBox(width: TSizes.spaceBtwItems),
-                          itemCount: 6,
-                          itemBuilder: (_, index) =>
-                          TRoundedImage(
-                        width: 80,
-                        backgroundColor: dark ? TColors.dark : TColors.white,
-                        border: Border.all(color: TColors.primary),
-                        padding: const EdgeInsets.all(TSizes.sm),
-                        imageUrl: TImages.productImage3,
-                      ),
+            TProductImageSlider(),
 
-                      ),
-                    ),
-                  ),
+            Padding(
+              padding: EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace, bottom: TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  TRatingAndShare(),
+
+                  TProductMetaData(),
                 ],
               ),
-            ),),
-
+            )
           ],
         ),
       ),
     );
   }
 }
+
+class TRatingAndShare extends StatelessWidget {
+  const TRatingAndShare({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            const Icon(Iconsax.star5, color: Colors.amber, size: 24,),
+         const SizedBox(width: TSizes.spaceBtwItems/2,),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(text: '5.0 ', style: Theme.of(context).textTheme.bodyLarge),
+                  const TextSpan(text: '(199)'),
+                ]
+              )
+            )
+          ],
+        ),
+        IconButton(onPressed: (){}, icon: const Icon(Icons.share, size: TSizes.iconMd,)),
+      ],
+    );
+  }
+}
+
