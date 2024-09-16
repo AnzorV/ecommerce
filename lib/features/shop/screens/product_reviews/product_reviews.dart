e17aa51ccc832aa09a05150a9e51cef330056753
AnzorV <anzorv@gmail.com>
2024-09-16 20:24:36 +0100

    master

--- lib/features/shop/screens/product_reviews/product_reviews.dart
+++ lib/features/shop/screens/product_reviews/product_reviews.dart
@@ -1,4 +1,7 @@
 import 'package:ecommerceshop/common/widgets/appbar/appbar.dart';
+import 'package:ecommerceshop/utils/constants/colors.dart';
+import 'package:ecommerceshop/utils/constants/sizes.dart';
+import 'package:ecommerceshop/utils/device/device_utility.dart';
 import 'package:flutter/material.dart';
 
 class ProductReviewsScreen extends StatelessWidget {
@@ -6,12 +9,65 @@ class ProductReviewsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TAppBar(title: Text('Reviews & Ratings',), showBackArrow: true,),
+    return Scaffold(
+      appBar: const TAppBar(title: Text('Reviews & Ratings',), showBackArrow: true,),
 
       body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Ratings and reviews are verified and are from people who use the same type of device that you use.'),
+            const SizedBox(height: TSizes.spaceBtwItems),
+          Row(
+            children: [
+              Expanded(flex: 3, child: Text('4.8', style: Theme.of(context).textTheme.displayLarge,)),
+
+              Expanded(
+                flex: 7,
+                child: Column(
+                  children: [
+                    TRatingProgressIndicator()
+                  ],
+                ),
+              )
+            ],
+          )
+          ],
+        ),
+
+        ),
 
       ),
+    );
+  }
+}
+
+class TRatingProgressIndicator extends StatelessWidget {
+  const TRatingProgressIndicator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(flex: 1, child: Text('5',style: Theme.of(context).textTheme.bodyMedium,)),
+        Expanded(
+          flex: 11,
+          child: SizedBox(
+            width: TDeviceUtils.getScreenWidth(context) * 0.5,
+            child: LinearProgressIndicator(
+              value: 0.5,
+              minHeight: 11,
+              backgroundColor: TColors.grey,
+              borderRadius: BorderRadius.circular(7),
+              valueColor: const AlwaysStoppedAnimation(TColors.primary),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
