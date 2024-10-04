6a945d6d57872d16c187429f2b003ca119ca8317
AnzorV <anzorv@gmail.com>
2024-10-04 19:03:52 +0100

    master

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerceshop/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:ecommerceshop/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerceshop/utils/theme/theme.dart';
 import 'package:get/get.dart';
@@ -13,7 +14,7 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home: const OnBoardingScreen(),
+      home: const Scaffold(backgroundColor: TColors.primary, body: Center(child: CircularProgressIndicator(color: Colors.white,),),),
     );
   }
 }
